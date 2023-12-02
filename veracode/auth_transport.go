6582e0dd0f64bee470bbffa8572cb2cb6b6be6b6
Daniel Stadler <danielvstadler@gmail.com>
2023-12-02 11:03:39 +0200

    Added comments

--- veracode/auth_transport.go
+++ veracode/auth_transport.go
@@ -12,6 +12,8 @@ type AuthTransport struct {
 	Transport http.RoundTripper
 }
 
+// RoundTrip is required to implement the http.RoundTripper interface.
+// It gets the Authorization value using the github.com/DanCreative/veracode-hmac-go module.
 func (t *AuthTransport) RoundTrip(req *http.Request) (*http.Response, error) {
 	bearer, err := vmac.CalculateAuthorizationHeader(req.URL, req.Method, t.key, t.secret)
 	if err != nil {
@@ -24,6 +26,7 @@ func (t *AuthTransport) RoundTrip(req *http.Request) (*http.Response, error) {
 	return t.transport().RoundTrip(req)
 }
 
+// transport checks if a custom http.RoundTripper was provided and returns it if it was and the http.DefaultTransport if it wasn't.
 func (t *AuthTransport) transport() http.RoundTripper {
 	if t.Transport != nil {
 		return t.Transport
@@ -31,10 +34,12 @@ func (t *AuthTransport) transport() http.RoundTripper {
 	return http.DefaultTransport
 }
 
+// Client() returns a new http.Client with this AuthTransport object set as its Transport.
 func (t *AuthTransport) Client() *http.Client {
 	return &http.Client{Transport: t}
 }
 
+// NewAuthTransport takes a object that implements interface http.RoundTripper as well as a Veracode API Key and Secret and returns a new AuthTransport.
 func NewAuthTransport(rt http.RoundTripper, key, secret string) (AuthTransport, error) {
 	t := AuthTransport{
 		Transport: rt,
