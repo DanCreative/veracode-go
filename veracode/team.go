664f066ffe63ba11b2e9b730e51f936cc1ebbf0a
Daniel Stadler <danielvstadler@gmail.com>
2023-12-03 22:51:26 +0200

    Fixed edge case with teams marshalling

--- veracode/team.go
+++ veracode/team.go
@@ -1,6 +1,8 @@
 package veracode
 
-import "encoding/json"
+import (
+	"encoding/json"
+)
 
 type Team struct {
 	TeamId       string           `json:"team_id,omitempty"`
@@ -13,7 +15,26 @@ type TeamRelationship struct {
 	Name string `json:"name,omitempty"`
 }
 
-func (t TeamRelationship) MarshalJSON() ([]byte, error) {
-	jsonValue, err := json.Marshal(t.Name)
-	return jsonValue, err
+// If Relationship.Name is "", create custom struct where TeamRelationship is a pointer and set it to nil.
+// This will omit relationship from the marshalled json.
+//
+// If Relationship.Name is not "", flatten TeamRelationship to Relationship in Team model.
+func (t *Team) MarshalJSON() ([]byte, error) {
+	type Alias Team
+	if t.Relationship.Name == "" {
+		return json.Marshal(&struct {
+			*Alias
+			Relationship *TeamRelationship `json:"relationship,omitempty"`
+		}{
+			Alias:        (*Alias)(t),
+			Relationship: nil,
+		})
+	}
+	return json.Marshal(&struct {
+		*Alias
+		Relationship string `json:"relationship,omitempty"`
+	}{
+		Alias:        (*Alias)(t),
+		Relationship: t.Relationship.Name,
+	})
 }
