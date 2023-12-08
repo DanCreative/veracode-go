c8b9c46d16be1dcf0774955e981feaa99d8fdf9e
Daniel Stadler <danielvstadler@gmail.com>
2023-12-08 21:51:04 +0200

    Added all functions for Business Units.

---
+++ veracode/business_unit.go
@@ -0,0 +1,142 @@
+package veracode
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"net/http"
+
+	"github.com/google/go-querystring/query"
+)
+
+type BusinessUnit struct {
+	BuId       string  `json:"bu_id,omitempty"`
+	BuLegacyId int     `json:"bu_legacy_id,omitempty"`
+	BuName     string  `json:"bu_name,omitempty"`
+	IsDefault  *bool   `json:"is_default,omitempty"`
+	Teams      *[]Team `json:"teams,omitempty"`
+}
+
+// buSearchResult is required to decode the list of business units and search user response bodies.
+type buSearchResult struct {
+	Embedded struct {
+		BusinessUnits []BusinessUnit `json:"business_units"`
+	} `json:"_embedded"`
+	Links navLinks `json:"_links"`
+	Page  pageMeta `json:"page"`
+}
+
+// ListBusinessUnits returns a list of business units. A name can optionally be provided to search for BUs by name.
+//
+// Veracode API documentation:
+//   - https://docs.veracode.com/r/c_identity_list_bu
+func (i *IdentityService) ListBusinessUnits(ctx context.Context, searchTerm string) ([]BusinessUnit, *http.Response, error) {
+	req, err := i.Client.NewRequest(ctx, "/business_units", http.MethodGet, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	if searchTerm != "" {
+		req.URL.RawQuery = "bu_name=" + searchTerm
+	}
+
+	var buResult buSearchResult
+
+	resp, err := i.Client.Do(req, &buResult)
+	if err != nil {
+		return nil, resp, err
+	}
+	return buResult.Embedded.BusinessUnits, resp, err
+}
+
+// GetBusinessUnit returns the BusinessUnit with the provided buId.
+//
+// Veracode API documentation:
+//   - https://docs.veracode.com/r/c_identity_bu_info
+func (i *IdentityService) GetBusinessUnit(ctx context.Context, buId string) (*BusinessUnit, *http.Response, error) {
+	req, err := i.Client.NewRequest(ctx, "/business_units/"+buId, http.MethodGet, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var getBu BusinessUnit
+
+	resp, err := i.Client.Do(req, &getBu)
+	if err != nil {
+		return nil, resp, err
+	}
+	return &getBu, resp, err
+}
+
+// CreateBusinessUnit creates a new bu using the provided BusinessUnit object.
+//
+// Veracode API documentation:
+//   - https://docs.veracode.com/r/c_identity_create_bu
+func (i *IdentityService) CreateBusinessUnit(ctx context.Context, bu *BusinessUnit) (*BusinessUnit, *http.Response, error) {
+	buf, err := json.Marshal(bu)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	req, err := i.Client.NewRequest(ctx, "/business_units", http.MethodPost, bytes.NewBuffer(buf))
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var newBu BusinessUnit
+	resp, err := i.Client.Do(req, &newBu)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return &newBu, resp, nil
+}
+
+// UpdateBusinessUnit updates a specific bu and sets nulls to fields not in the request (if the database allows it) unless partial is set to true.
+// If incremental is set to true, any values in the teams list will be added to the bu's teams instead of replacing them.
+//
+// Veracode API documentation:
+//   - https://docs.veracode.com/r/c_identity_update_bu
+//   - https://docs.veracode.com/r/c_identity_add_team_bu
+func (i *IdentityService) UpdateBusinessUnit(ctx context.Context, bu *BusinessUnit, options UpdateOptions) (*BusinessUnit, *http.Response, error) {
+	buf, err := json.Marshal(bu)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	req, err := i.Client.NewRequest(ctx, "/business_units/"+bu.BuId, http.MethodPut, bytes.NewBuffer(buf))
+	if err != nil {
+		return nil, nil, err
+	}
+
+	values, err := query.Values(options)
+	if err != nil {
+		return nil, nil, err
+	}
+	req.URL.RawQuery = values.Encode()
+
+	var updatedBu BusinessUnit
+	resp, err := i.Client.Do(req, &updatedBu)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return &updatedBu, resp, nil
+}
+
+// DeleteBusinessUnit deletes a bu from the Veracode API using the provided buId.
+//
+// Veracode API documentation:
+//   - https://docs.veracode.com/r/c_identity_delete_bu
+func (i *IdentityService) DeleteBusinessUnit(ctx context.Context, buId string) (*http.Response, error) {
+	req, err := i.Client.NewRequest(ctx, "/business_units/"+buId, http.MethodDelete, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := i.Client.Do(req, nil)
+	if err != nil {
+		return nil, err
+	}
+	return resp, err
+}
