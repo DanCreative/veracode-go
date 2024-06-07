042fa87d96891e94241ceea24d036a4070ed74e9
Daniel Stadler <danielvstadler@gmail.com>
2024-06-08 01:00:30 +0200

    Can now safely change the Client's baseURL field.

--- veracode/veracode.go
+++ veracode/veracode.go
@@ -8,10 +8,12 @@ import (
 	"net/http"
 	"net/url"
 	"strings"
+	"sync"
 )
 
 type Client struct {
-	BaseURL    *url.URL
+	baseURL    *url.URL
+	rwMu       sync.RWMutex
 	HttpClient *http.Client
 
 	// Services used for talking to the different parts of the Veracode API
@@ -45,7 +47,7 @@ func NewClient(region Region, httpClient *http.Client, apiKey, apiSecret string)
 	}
 
 	c := &Client{
-		BaseURL:    baseEndpoint,
+		baseURL:    baseEndpoint,
 		HttpClient: httpClient,
 	}
 
@@ -60,7 +62,11 @@ func (c *Client) NewRequest(ctx context.Context, endpoint string, method string,
 	if err != nil {
 		return nil, err
 	}
-	url := c.BaseURL.ResolveReference(urlEndpoint)
+
+	c.rwMu.RLock()
+	defer c.rwMu.RUnlock()
+
+	url := c.baseURL.ResolveReference(urlEndpoint)
 
 	req, err := http.NewRequestWithContext(ctx, method, url.String(), body)
 	if err != nil {
@@ -89,6 +95,20 @@ func (c *Client) Do(req *http.Request, body any) (*Response, error) {
 		}
 	}
 	return newResponse(resp, body), nil
+}
+
+// SetBaseURL takes a Region and updates the Client's baseURL. It write locks the sync.RWMutex to prevent any concurrent reads while writing (just in case).
+func (c *Client) SetBaseURL(region Region) error {
+	c.rwMu.Lock()
+	defer c.rwMu.Unlock()
+
+	new, err := url.Parse(string(region))
+	if err != nil {
+		return fmt.Errorf("error occured while trying to parse new base URL: %s", region)
+	}
+
+	c.baseURL = new
+	return nil
 }
 
 func newResponse(response *http.Response, body any) *Response {
