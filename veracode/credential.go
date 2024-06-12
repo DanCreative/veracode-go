4536c25120fbbda967166c11593f0ecb5c18d702
Daniel Stadler <danielvstadler@gmail.com>
2024-06-12 22:52:49 +0200

    Added functionality to get a list of the valid profiles from the Veracode credentials file.

--- veracode/credential.go
+++ veracode/credential.go
@@ -9,14 +9,61 @@ import (
 	"gopkg.in/ini.v1"
 )
 
-// getCredentialsFilePath gets the Veracode API credentials file path.
-func getCredentialsFilePath() (string, error) {
+type Profile struct {
+	Name                 string
+	VeracodeApiKeyId     string
+	VeracodeApiKeySecret string
+}
+
+// GetCredentialsFilePath gets the Veracode API credentials file path.
+func GetCredentialsFilePath() (string, error) {
 	homeDir, err := os.UserHomeDir()
 	if err != nil {
 		return "", err
 	}
 
 	return path.Join(homeDir, ".veracode", "credentials"), nil
+}
+
+// GetProfiles returns all of the profiles stored in the Veracode credentials file.
+func GetProfiles(filePath string) (map[string]Profile, error) {
+	cfg, err := ini.Load(filePath)
+	if err != nil {
+		return nil, fmt.Errorf("error loading ini file. Message: %s", err.Error())
+	}
+
+	sections := cfg.Sections()
+	profiles := make(map[string]Profile)
+
+	for _, section := range sections {
+		if validProfile, ok := sectionToValidProfile(section); ok {
+			profiles[validProfile.Name] = validProfile
+		}
+	}
+
+	return profiles, nil
+}
+
+// sectionToValidProfile converts an ini section to a Profile and returns a bool indicating
+// whether the given Profile is valid or not.
+func sectionToValidProfile(section *ini.Section) (Profile, bool) {
+	p := Profile{
+		Name: section.Name(),
+	}
+
+	if !section.HasKey("veracode_api_key_id") {
+		return Profile{}, false
+
+	}
+
+	if !section.HasKey("veracode_api_key_secret") {
+		return Profile{}, false
+	}
+
+	p.VeracodeApiKeyId = section.Key("veracode_api_key_id").String()
+	p.VeracodeApiKeySecret = section.Key("veracode_api_key_secret").String()
+
+	return p, true
 }
 
 // GetProfile returns a pointer to the section of the credentials file that the user is using.
@@ -57,7 +104,7 @@ func getProfile(filePath string) (*ini.Section, error) {
 // The credentials file should be in the .ini format and should be present in the /.veracode/ folder in the user's home
 // directory. Please refer to the documentation for more information: https://docs.veracode.com/r/c_httpie_tool.
 func LoadVeracodeCredentials() (string, string, error) {
-	credsPath, err := getCredentialsFilePath()
+	credsPath, err := GetCredentialsFilePath()
 	if err != nil {
 		return "", "", err
 	}
