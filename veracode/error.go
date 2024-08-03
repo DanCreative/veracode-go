7e381892971fde77367cbe6813a658f4e339cc9e
Daniel Stadler <danielvstadler@gmail.com>
2024-08-03 02:14:11 +0200

    Added support for unmarshalling all of the different error models that can be returned by the APIs.

--- veracode/error.go
+++ veracode/error.go
@@ -8,7 +8,7 @@ import (
 	"strings"
 )
 
-// The Veracode APIs can return 2 different error response bodies.
+// The Veracode APIs can return multiple different error response bodies.
 //
 // Example of a general error:
 //
@@ -27,24 +27,105 @@ import (
 //	  ],
 //	  "status": 400
 //	}
+// Example of an error returned by the Applications API:
+//
+// {
+//     "_embedded" : {
+//       "api_errors" : [ {
+//         "id" : "abcd",
+//         "code" : "NOT_FOUND",
+//         "title" : "The requested application could not be found",
+//         "status" : "404",
+//         "source" : {
+//           "pointer" : "/appsec/v1/applications/abcd",
+//           "parameter" : null
+//         }
+//       } ]
+//     },
+//     "fallback_type" : null,
+//     "full_type" : null
+// }
+//
+// {
+// 	"id" : "blah",
+// 	"code" : "BAD_REQUEST",
+// 	"title" : "Failed to convert value of type 'java.lang.String' to required type 'com.veracode.agora.collection.domain.CollectionComplianceStatus'; nested exception is org.springframework.core.convert.ConversionFailedException: Failed to convert from type [java.lang.String] to type [@io.swagger.v3.oas.annotations.Parameter @org.springframework.web.bind.annotation.RequestParam com.veracode.agora.collection.domain.CollectionComplianceStatus] for value 'blah'; nested exception is java.lang.IllegalArgumentException: No enum constant com.veracode.agora.collection.domain.CollectionComplianceStatus.blah",
+// 	"detail" : null,
+// 	"status" : "400",
+// 	"source" : {
+// 	  "pointer" : "/appsec/v1/collections",
+// 	  "parameter" : null
+// 	}
+//  }
+
 type Error struct {
+	Code     int
+	Endpoint string
+	Messages []string
+}
+
+func (v Error) Error() string {
+	return fmt.Sprintf("api error returned from %s (%d): [\"%s\"]", v.Endpoint, v.Code, strings.Join(v.Messages, "\", \""))
+}
+
+func (e *Error) UnmarshalJSON(data []byte) (err error) {
+	errBody := errorBody{}
+	err = json.Unmarshal(data, &errBody)
+	if err != nil {
+		return
+	}
+
+	if errBody.Title != "" {
+		e.Messages = []string{errBody.Title}
+		return
+	}
+
+	if errBody.Message != "" {
+		e.Messages = []string{errBody.Message}
+		return
+	}
+
+	if l := len(errBody.Errors); l > 0 {
+		e.Messages = make([]string, l)
+		copy(e.Messages, errBody.Errors)
+		return
+	}
+
+	if l := len(errBody.Embedded.Errors); l > 0 {
+		e.Messages = make([]string, len(errBody.Embedded.Errors))
+		for k, apiError := range errBody.Embedded.Errors {
+			e.Messages[k] = apiError.Title
+		}
+		return
+	}
+
+	return
+}
+
+type errorBody struct {
 	HttpCode   int    `json:"http_code"`
 	HttpStatus string `json:"http_status"`
 	Message    string `json:"message"`
 
+	Title  string   `json:"title,omitempty"`
 	Errors []string `json:"errors"`
-	Status int      `json:"status"`
+	// Status int      `json:"status"`
+
+	Embedded struct {
+		Errors []struct {
+			Id     string `json:"id,omitempty"`
+			Code   string `json:"code,omitempty"`
+			Title  string `json:"title,omitempty"`
+			Status string `json:"status,omitempty"`
+			Source struct {
+				Pointer   string `json:"pointer,omitempty"`
+				Parameter string `json:"parameter,omitempty"`
+			} `json:"source,omitempty"`
+		} `json:"api_errors,omitempty"`
+	} `json:"_embedded,omitempty"`
 }
 
-func (v *Error) Error() string {
-	if len(v.Errors) == 0 {
-		return fmt.Sprintf("status: %d; message: %s", v.HttpCode, v.Message)
-	} else {
-		return fmt.Sprintf("status: %d; messages: [\"%s\"]", v.Status, strings.Join(v.Errors, "\", \""))
-	}
-}
-
-// NewVeracodeError unmarshalls a response body into a new Veracode error.
+// NewVeracodeError unmarshals a response body into a new Veracode error.
 func NewVeracodeError(resp *http.Response) error {
 	if resp == nil {
 		return fmt.Errorf("failed to create new Veracode Error because response is empty")
@@ -55,8 +136,8 @@ func NewVeracodeError(resp *http.Response) error {
 		return err
 	}
 
-	verr := &Error{}
-	err = json.Unmarshal(body, verr)
+	verr := Error{Code: resp.StatusCode, Endpoint: resp.Request.URL.Path}
+	err = json.Unmarshal(body, &verr)
 	if err != nil {
 		return err
 	}
